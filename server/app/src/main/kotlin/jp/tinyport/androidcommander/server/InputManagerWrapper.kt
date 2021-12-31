a7f96307a6c1ab3801de6f460c957dc00073258f
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2021-12-31 21:47:42 +0900

    Implement key event server

---
+++ server/app/src/main/kotlin/jp/tinyport/androidcommander/server/InputManagerWrapper.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 sukawasatoru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.tinyport.androidcommander.server
+
+import android.os.IInterface
+import android.view.InputEvent
+
+class InputManagerWrapper(private val manager: IInterface) {
+    companion object {
+        const val INJECT_INPUT_EVENT_MODE_ASYNC = 0
+        const val INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT = 1
+        const val INJECT_INPUT_EVENT_MODE_WAIT_FOR_FINISH = 2
+    }
+
+    private val injectInputEventMethod = manager
+            .javaClass
+            .getMethod("injectInputEvent", InputEvent::class.java, Int::class.java)
+
+    fun injectInputEvent(event: InputEvent?, mode: Int): Boolean {
+        return injectInputEventMethod.invoke(manager, event, mode) as Boolean
+    }
+}
--- server/app/src/main/kotlin/jp/tinyport/androidcommander/server/Main.kt
+++ server/app/src/main/kotlin/jp/tinyport/androidcommander/server/Main.kt
@@ -16,6 +16,97 @@
 
 package jp.tinyport.androidcommander.server
 
-fun main(args: Array<String>?) {
-    println("Hello!")
+import android.os.SystemClock
+import android.text.TextUtils
+import android.view.KeyEvent
+import java.util.Scanner
+
+fun main() {
+    println("Hello")
+
+    val serviceManager = ServiceManagerWrapper()
+    val inputManager = serviceManager.inputManager
+    val scanner = Scanner(System.`in`)
+
+    var lastCommand: ClientCommand? = null
+    var repeatCount = 0
+
+    while (true) {
+        val command = try {
+            parseLine(scanner.nextLine()) ?: continue
+        } catch (e: NoSuchElementException) {
+            break
+        }
+
+        if (command.action == KeyAction.Down && command == lastCommand) {
+            repeatCount += 1
+        } else {
+            repeatCount = 0
+        }
+
+        val eventTime = SystemClock.uptimeMillis()
+        inputManager.injectInputEvent(
+            KeyEvent(
+                eventTime,
+                eventTime,
+                command.action.code,
+                command.code.code,
+                repeatCount
+            ),
+            InputManagerWrapper.INJECT_INPUT_EVENT_MODE_ASYNC,
+        )
+
+        lastCommand = command
+    }
+
+    println("Bye")
 }
+
+private fun parseLine(line: String): ClientCommand? {
+    val lineSegments = TextUtils.split(line, " ")
+    if (lineSegments.size != 2) {
+        println("unexpected format: $line")
+        return null
+    }
+
+    val (actionString, codeString) = lineSegments
+
+    return ClientCommand(
+        action = KeyAction.from(actionString) ?: run {
+            println("unexpected action: $actionString")
+            return null
+        },
+        code = KeyCode.from(codeString) ?: run {
+            println("unexpected code: $codeString")
+            return null
+        },
+    )
+}
+
+enum class KeyAction {
+    Down,
+    Up;
+
+    companion object {
+        fun from(value: String): KeyAction? {
+            return values().find { it.id == value }
+        }
+    }
+
+    val id: String
+        get() = when (this) {
+            Down -> "down"
+            Up -> "up"
+        }
+
+    val code: Int
+        get() = when (this) {
+            Down -> KeyEvent.ACTION_DOWN
+            Up -> KeyEvent.ACTION_UP
+        }
+}
+
+data class ClientCommand(
+    val action: KeyAction,
+    val code: KeyCode,
+)
