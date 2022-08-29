09b543b64ea21f919925149bc06198163ed4f248
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2022-08-29 09:58:34 +0900

    Fix commit file missing

--- client/src/feature/settings.rs
+++ client/src/feature/settings.rs
@@ -14,121 +14,166 @@
  * limitations under the License.
  */
 
-use crate::model::app_command::AppCommand;
+use crate::data::preferences_repository::PreferencesRepository;
 use crate::model::ButtonStyle;
-use iced::widget::{button, column, text, Column};
+use crate::model::{AppTheme, XMessage};
+use crate::prelude::Fallible;
+use iced::widget::{button, column, pick_list, row, text, Column};
 use iced::{Command, Element};
+use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
-use tokio::sync::broadcast::Sender;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use tracing::{debug, warn};
 
-pub struct SettingsView {
-    common_command_tx: Sender<AppCommand>,
+pub struct ViewState {
     config_file_path: PathBuf,
+    theme: AppTheme,
 }
 
-impl SettingsView {
-    pub fn new(common_command_tx: Sender<AppCommand>, config_file_path: PathBuf) -> Self {
+impl ViewState {
+    pub fn new(config_file_path: PathBuf, theme: AppTheme) -> Self {
         Self {
-            common_command_tx,
             config_file_path,
+            theme,
         }
     }
 }
 
 #[derive(Clone, Debug)]
 pub enum SettingsViewCommand {
+    OnThemeSelected(AppTheme),
     OnOpenKeycodeReferencesButtonClicked,
     OnOpenPrefsButtonClicked,
     OnOpenPrefsDirButtonClicked,
-    OnReloadPrefsButtonClicked,
+    OnXMessage(XMessage),
+    SendXMessage(XMessage),
+    Sink,
 }
 
-impl SettingsView {
-    pub fn update(&mut self, command: SettingsViewCommand) -> Command<SettingsViewCommand> {
+pub trait SettingsView {
+    type PrefsRepo: PreferencesRepository + 'static;
+
+    fn get_prefs_repo(&self) -> Arc<Mutex<Self::PrefsRepo>>;
+
+    fn get_state(&self) -> &ViewState;
+
+    fn get_state_mut(&mut self) -> &mut ViewState;
+
+    fn update(&mut self, command: SettingsViewCommand) -> Command<SettingsViewCommand> {
         match command {
-            SettingsViewCommand::OnOpenPrefsButtonClicked => self.open_prefs(),
-            SettingsViewCommand::OnOpenPrefsDirButtonClicked => self.open_prefs_directory(),
+            SettingsViewCommand::OnThemeSelected(theme) => {
+                return Command::perform(
+                    save_theme(self.get_prefs_repo(), theme),
+                    |data| match data {
+                        Ok(_) => SettingsViewCommand::SendXMessage(XMessage::OnPrefsFileUpdated),
+                        Err(e) => {
+                            warn!(?e, "failed to save theme");
+                            SettingsViewCommand::Sink
+                        }
+                    },
+                );
+            }
+            SettingsViewCommand::OnOpenPrefsButtonClicked => open_prefs(self.get_state()),
+            SettingsViewCommand::OnOpenPrefsDirButtonClicked => {
+                open_prefs_directory(self.get_state())
+            }
             SettingsViewCommand::OnOpenKeycodeReferencesButtonClicked => open_keycode_references(),
-            SettingsViewCommand::OnReloadPrefsButtonClicked => {
-                self.common_command_tx
-                    .send(AppCommand::OnPrefsFileUpdated)
-                    .ok();
+            SettingsViewCommand::OnXMessage(data) => match data {
+                XMessage::OnPrefsFileUpdated => {
+                    // do nothing.
+                }
+                XMessage::OnNewPreferences(prefs) => {
+                    self.get_state_mut().theme = prefs.theme;
+                }
+            },
+            SettingsViewCommand::SendXMessage(_) | SettingsViewCommand::Sink => {
+                // do nothing.
             }
         }
 
         Command::none()
     }
 
-    pub fn view<'a, Theme>(&'a self) -> Element<'a, SettingsViewCommand, iced::Renderer<Theme>>
+    fn view<'a, Theme>(&'a self) -> Element<'a, SettingsViewCommand, iced::Renderer<Theme>>
     where
         Theme: button::StyleSheet<Style = ButtonStyle> + 'a,
+        Theme: pick_list::StyleSheet,
         Theme: text::StyleSheet,
     {
         let view: Column<SettingsViewCommand, iced::Renderer<Theme>> = column![
-            button("Reload preferences")
-                .width(292.into())
-                .on_press(SettingsViewCommand::OnReloadPrefsButtonClicked),
+            button("Reload preferences").width(292.into()).on_press(
+                SettingsViewCommand::SendXMessage(XMessage::OnPrefsFileUpdated)
+            ),
             button("Open preferences directory")
                 .width(292.into())
                 .on_press(SettingsViewCommand::OnOpenPrefsDirButtonClicked),
             button("Open KeyCode references")
                 .width(292.into())
                 .on_press(SettingsViewCommand::OnOpenKeycodeReferencesButtonClicked),
+            row![
+                "Theme: ",
+                pick_list(
+                    &[AppTheme::Light, AppTheme::Dark][..],
+                    Some(self.get_state().theme),
+                    SettingsViewCommand::OnThemeSelected,
+                ),
+            ]
+            .align_items(iced::alignment::Alignment::Center),
         ]
         .spacing(8);
         view.into()
     }
 
-    pub fn view_size(&self) -> (u32, u32) {
+    fn view_size(&self) -> (u32, u32) {
         (300, 260)
     }
+}
 
-    fn open_prefs(&self) {
-        if let Ok(visual) = std::env::var("VISUAL") {
-            debug!(%visual, "use VISUAL");
+fn open_prefs(state: &ViewState) {
+    if let Ok(visual) = std::env::var("VISUAL") {
+        debug!(%visual, "use VISUAL");
 
-            let visual_result = std::process::Command::new(visual)
-                .arg(self.config_file_path.as_os_str())
-                .spawn();
-
-            if visual_result.is_ok() {
-                debug!("succeeded");
-                return;
-            }
-        }
-
-        let filer = get_filer();
-
-        debug!(%filer, "use filer");
-        let filer_result = std::process::Command::new(filer)
-            .arg(self.config_file_path.as_os_str())
+        let visual_result = std::process::Command::new(visual)
+            .arg(state.config_file_path.as_os_str())
             .spawn();
 
-        if filer_result.is_ok() {
+        if visual_result.is_ok() {
             debug!("succeeded");
             return;
         }
-
-        warn!(?filer_result, %filer, "failed to open preferences");
     }
 
-    fn open_prefs_directory(&self) {
-        let filer = get_filer();
+    let filer = get_filer();
 
-        let dir = match self.config_file_path.parent() {
-            Some(data) => data,
-            None => return,
-        };
+    debug!(%filer, "use filer");
+    let filer_result = std::process::Command::new(filer)
+        .arg(state.config_file_path.as_os_str())
+        .spawn();
 
-        let ret = std::process::Command::new(filer)
-            .arg(dir.as_os_str())
-            .spawn();
+    if filer_result.is_ok() {
+        debug!("succeeded");
+        return;
+    }
 
-        match ret {
-            Ok(_) => debug!("succeeded"),
-            Err(e) => warn!(?e, %filer, "failed to open directory"),
-        }
+    warn!(?filer_result, %filer, "failed to open preferences");
+}
+
+fn open_prefs_directory(state: &ViewState) {
+    let filer = get_filer();
+
+    let dir = match state.config_file_path.parent() {
+        Some(data) => data,
+        None => return,
+    };
+
+    let ret = std::process::Command::new(filer)
+        .arg(dir.as_os_str())
+        .spawn();
+
+    match ret {
+        Ok(_) => debug!("succeeded"),
+        Err(e) => warn!(?e, %filer, "failed to open directory"),
     }
 }
 
@@ -152,5 +197,27 @@ fn open_keycode_references() {
     match ret {
         Ok(_) => debug!("succeeded"),
         Err(e) => warn!(?e, %filer, "failed to open keycode references"),
+    }
+}
+
+async fn save_theme<Repo: PreferencesRepository>(
+    repo: Arc<Mutex<Repo>>,
+    theme: AppTheme,
+) -> Fallible<()> {
+    let repo = repo.lock().await;
+    let mut prefs = match repo.load().await {
+        Ok(data) => data,
+        Err(e) => return Err(e),
+    };
+    prefs.theme = theme;
+    repo.save(prefs).await
+}
+
+impl Display for AppTheme {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AppTheme::Light => write!(f, "Light"),
+            AppTheme::Dark => write!(f, "Dark"),
+        }
     }
 }
